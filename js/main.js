24c42cd0a7913f77f53a1e8bb01934dd6230d49d
Gustavo Costa <cmtehenz@gmail.com>
2018-02-25 19:05:52 -0300

    salvando registros local storage

--- js/main.js
+++ js/main.js
@@ -27,6 +27,7 @@ function setList(list){
 
     document.getElementById('listTable').innerHTML = table
     getTotal(list)
+    saveListStorage(list)
 }
 
 function formatDesc(desc){
@@ -167,4 +168,18 @@ function deleteList(){
     }
 }
 
-setList(list)
+function saveListStorage(list){
+    var jsonStr = JSON.stringify(list);
+    localStorage.setItem('list', jsonStr);
+}
+
+function initListStorage(){
+    var testList = localStorage.getItem('list');
+    if(testList){
+        list = JSON.parse(testList)
+    }
+    
+    setList(list)
+}
+
+initListStorage()
